a0edb5e53f967a6abd9f998f88b7fa8300bdcfe8
Praveen Seneviratne <63094219+Gudnite@users.noreply.github.com>
2021-08-17 20:16:01 +0530

    Complete system

---
+++ src/cvproject/mains.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cvproject;
+
+
+
+/**
+ *
+ * @author Asus
+ */
+public class mains {
+    private String firstname;
+    private String lastname;
+    private String address;
+    public String gender;
+    
+   
+}
